9a1f5325861a68843528e19adb6a9f9133365574
Daniel Sposito <daniel.g.sposito@gmail.com>
2018-06-28 04:30:51 -0700

    Standardizes on double quotes for consistency.

--- models/tube.py
+++ models/tube.py
@@ -24,6 +24,6 @@ class TubeStatus(str, Enum):
 
 class TubeSchema(Schema):
     id = fields.Int(description="The tube's unique identifier.", example=35, required=True)
-    name = fields.Str(description="The tube's name.", example='CA-NT1', required=True)
+    name = fields.Str(description="The tube's name.", example="CA-NT1", required=True)
     type = fields.Str(description="The tube's type.", enum=list(TubeType), example=TubeType.TUNNEL, required=True)
     status = fields.Str(description="The tube's status.", enum=list(TubeStatus), example=TubeStatus.ACTIVE, required=True)
