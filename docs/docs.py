531ebef441fe18ba6b436b3f72fc43c6b8d8f53a
Daniel Sposito <daniel.g.sposito@gmail.com>
2018-07-06 03:49:11 -0700

    Adds documentation builder that uses APISpec to convert marshmallow schema objects to their OpenAPI YAML representation. Allows for auto documentation generation from model schemas.

--- docs/docs.py
+++ docs/docs.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
 
+from docs.openapi import OpenAPI
+
+import os
+
 docs = Blueprint("docs", "docs", url_prefix="/docs", template_folder="templates", static_folder="static")
 
 @docs.route("/")
 def index():
+    # Keep docs up-to-date by rebuilding on each page load.
+    if os.environ.get("FLASK_ENV") == "development":
+        OpenAPI.buildSpec()
+
     return render_template("index.html")
---
+++ docs/openapi.py
@@ -0,0 +1,33 @@
+from apispec.ext.marshmallow.swagger import schema2jsonschema
+
+from models.pod import PodSchema
+from models.station import StationSchema
+from models.tube import TubeSchema
+
+import json, yaml
+
+class OpenAPI():
+    # Export each model's schema to YAML for API documentation.
+    # Export final YAML spec to JSON for ReDoc documentation UI.
+    def buildSpec():
+        schemas = {
+            "pod": PodSchema(),
+            "station": StationSchema(),
+            "tube": TubeSchema(),
+        }
+
+        for name, schema in schemas.items():
+            # Convert each schema class to a dictionary.
+            content = schema2jsonschema(schema)
+
+            # Convert dictionary to valid JSON string and back again. Without this conversion,
+            # YAML output has !!python tags and extra "dictitems" key within "properties".
+            content = json.loads(json.dumps(content))
+
+            with open("docs/static/openapi/schemas/" + name + ".yaml", "w") as file:
+                yaml.dump(content, file, default_flow_style=False, allow_unicode=True)
+
+        # Export combined YAML to JSON for ReDoc.
+        spec = open("docs/static/openapi/spec.yaml", "r")
+        with open("docs/static/openapi/spec.json", "w") as f:
+            json.dump(yaml.load(spec), f)
