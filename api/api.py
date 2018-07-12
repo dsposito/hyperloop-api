c3b7f7e545a49acbfa2860520d4e874b81878965
Daniel Sposito <daniel.g.sposito@gmail.com>
2018-07-12 01:00:28 -0700

    Standardizes API response code formatting.

--- api/api.py
+++ api/api.py
@@ -12,7 +12,7 @@ def stations():
     stations = schema.dump([
         Station(1, StationType.PUBLIC, "HAW", 33.920659, -118.328278, StationStatus.ACTIVE),
         Station(1900, StationType.PUBLIC, "FRE", 37.492509, -121.944616, StationStatus.INACTIVE),
-        Station(6800, StationType.PRIVATE, "EMD", 35.671724, -97.508266, StationStatus.ACTIVE)
+        Station(6800, StationType.PRIVATE, "EMD", 35.671724, -97.508266, StationStatus.ACTIVE),
     ])
 
     return jsonify(stations.data)
@@ -32,7 +32,7 @@ def tubes():
     tubes = schema.dump([
         Tube(15, TubeType.TUNNEL, "CA", TubeDirection.NORTH, 1, TubeStatus.ACTIVE),
         Tube(35, TubeType.TUNNEL, "WA", TubeDirection.SOUTH, 1, TubeStatus.INACTIVE),
-        Tube(85, TubeType.ELEVATED, "CA", TubeDirection.WEST, 2, TubeStatus.ACTIVE)
+        Tube(85, TubeType.ELEVATED, "CA", TubeDirection.WEST, 2, TubeStatus.ACTIVE),
     ])
 
     return jsonify(tubes.data)
@@ -52,6 +52,7 @@ def pods():
     pods = schema.dump([
         Pod(2000, PodType.PASSENGER, PodStatus.ACTIVE),
         Pod(3005, PodType.CARGO, PodStatus.ACTIVE),
+        Pod(6010, PodType.VEHICLE, PodStatus.INACTIVE),
     ])
 
     return jsonify(pods.data)
